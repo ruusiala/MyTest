ea50b0386e057e45cda146f2ebd50d6efc39ca1e
ruusiala <ruusiala@gmail.com>
2014-12-12 12:42:46 +0200

    Project skeleton

---
+++ SpringTest/src/main/java/com/jarmo/app/App.java
@@ -0,0 +1,13 @@
+package com.jarmo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
